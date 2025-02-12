cc4adb31f2928797b6c4db92a4016715330e6cc6
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-02-12 08:54:19 -0300

    testing

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+// Routes
+const routes = require('./src/routes/routes')
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}))
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Bem vindo ao servidor' })
+})
+
+app.use(routes)
+
+module.exports = app
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,3 +1,13 @@
-require('dotenv').config({ path: 'env.test' })
+require('dotenv').config({ path: '.env.test' })
 
 const request = require('supertest')
+const app = require('../app')
+
+
+test('Deve criar conta de usuário', async () => {
+    await request(app).post('/signup').send({
+        userName: 'Caio',
+        email: 'Caio@gmail.com',
+        password: '123456'
+    }).expect(201)
+})
